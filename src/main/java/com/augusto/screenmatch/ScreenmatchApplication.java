b29f1434d50466f52be9abb0f478364f123cd349
Augusto Souza <augusto.souzaco@gmail.com>
2025-03-06 18:21:58 -0300

    Add files

--- src/main/java/com/augusto/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/augusto/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package com.augusto.screenmatch;
 
+import com.augusto.screenmatch.model.DadosEpisodio;
 import com.augusto.screenmatch.model.DadosSerie;
+import com.augusto.screenmatch.model.DadosTemporada;
 import com.augusto.screenmatch.service.ConsumoApi;
 import com.augusto.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,9 +23,25 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=breaking+bad&apikey=d40064cb");
+
 		System.out.println(json);
+
 		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+
+		System.out.println(dadosSerie);
+
+		json = consumoApi.obterDados("http://www.omdbapi.com/?t=breaking+bad&Season=1&episode=2&apikey=d40064cb");
+		DadosEpisodio dadosEp = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEp);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i = 1; i<=dadosSerie.temporadas(); i++) { //Começa no 1 pq não tem temp 0
+			json = consumoApi.obterDados("http://www.omdbapi.com/?t=breaking+bad&Season=" + i + "&apikey=d40064cb");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println); // Method reference (forma curta de escrever uma expressão lambda,
+		// metodo forEach pertence a interface iterable
 	}
 }
